5b0a97cc938b31a4b225b6428f3f5483b9e9d499
Gabe <28gabe@gmail.com>
2024-06-19 11:24:08 -0700

    create data service interface

---
+++ src/interfaces/data-service.ts
@@ -0,0 +1,14 @@
+import { TaskType } from '@/types/task';
+import { QuickLinkType } from '@/types/quick-link';
+
+export interface IDataService {
+  fetchTasks(): Promise<TaskType[]>;
+  addTask(task: TaskType): Promise<TaskType>;
+  updateTask(task: TaskType): Promise<TaskType>;
+  deleteTask(id: number): Promise<void>;
+
+  fetchQuickLinks(): Promise<QuickLinkType[]>;
+  addQuickLink(quickLink: QuickLinkType): Promise<QuickLinkType>;
+  updateQuickLink(quickLink: QuickLinkType): Promise<QuickLinkType>;
+  deleteQuickLink(id: number): Promise<void>;
+}
